d6b12e5295ce38354e6f726c4e5e0a4e227ee267
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-02-16 18:41:06 +0300

    Create 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module 1-concat.py: concatenated two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return concatenated of two strings."""
+    return str1 + str2
