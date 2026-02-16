6b3ae200f4764ab25f017450bd00d3c4d7c80fac
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-02-16 18:47:20 +0300

    Update 2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+import math
 """Module 2-floor.py:  floor the float."""
 
 
 def floor(n: float) -> int:
     """Return floor of the float"""
-    return math.floor.(n)
+    return math.floor(n)
