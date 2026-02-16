2151d45e46b5313a4084f803a230d5ff953c83ae
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-02-16 18:58:33 +0300

    Create 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module 3-to_str.py: provides a function to return the string representation of the float."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float."""
+    return str(n)
