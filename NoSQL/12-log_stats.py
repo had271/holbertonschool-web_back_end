caf9e288c7b15b63b3950db642bd3abc9418d6a7
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-03-03 02:09:07 +0300

    Refactor log function to log_stats and improve output

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python3
-"""
-Script that provides stats about Nginx logs stored in MongoDB
-"""
-
+"""Script that provides stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
-def log():
-    """
-    Display stats about Nginx logs stored in MongoDB
-    """
+def log_stats():
+    """Display stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
+    collection = client.logs.nginx
 
-    count = collection.count_documents({})
-    print(f"{count} logs")
-
+    print(f"{collection.count_documents({})} logs")
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-
-    status_checks = collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-    print(f"{status_checks} status check")
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status} status check")
 
 
 if __name__ == "__main__":
-    log()
+    log_stats()
