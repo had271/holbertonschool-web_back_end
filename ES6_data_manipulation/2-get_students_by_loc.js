4d6dc6c0f75ba96027022e14d75a69b31e7a4c3a
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-03-29 12:48:30 +0300

    Create 2-get_students_by_loc.js

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+// Filter based in the search of the city
+
+const getStudentsByLocation = (students, city) => {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students.filter((student) => student.location === city);
+};
+
+export default getStudentsByLocation;
