90a841b14328f87258d313f6a14667d5c85f0018
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-03-29 12:45:36 +0300

    Edit 1-get_list_student_ids.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 // List students by id
 
-export default function getListStudentIds = (students) => {
+export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
