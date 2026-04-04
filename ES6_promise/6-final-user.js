ff1fc0c6ae74cf43e1679e6a2b62e53086f81b50
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-04-04 19:43:49 +0300

    Refactor handleProfileSignup to handle promise results
    Refactor handleProfileSignup to process results of promises and map rejected statuses.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,7 +3,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([signUpUser(firstName, lastName),uploadPhoto(fileName)]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((res) => {
+      if (res.status === 'rejected') {
+        return {
+          status: 'rejected',
+          value: res.reason.toString(),
+        };
+      }
+      return res;
+    })
+  );
 }
-
-export default handleProfileSignup;
